27d49466b263d8353a5a7e0d220cbc1c35fc6e63
Decentnamed <dawid.grabowicz20@gmail.com>
2022-06-28 11:52:10 +0200

    Zadanie4

---
+++ OknoGrupy.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Zadanie4
+{
+    /// <summary>
+    /// Logika interakcji dla klasy OknoGrupy.xaml
+    /// </summary>
+    public partial class OknoGrupy : Window
+    {
+        public OknoGrupy(XmlNode grupa)
+        {
+            DataContext = grupa;
+            InitializeComponent();
+        }
+        private void WidokGrupy(object sender, RoutedEventArgs e)
+        {
+            ListBox lista = (ListBox)FindName("ListaPojazdów");
+            XmlNode produkt = (XmlNode)lista.SelectedItem;
+            if (produkt != null)
+                new OknoPojazdu(produkt).Show();
+        }
+    }
+}
---
+++ OknoPojazdu.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Zadanie4
+{
+    /// <summary>
+    /// Logika interakcji dla klasy OknoPojazdu.xaml
+    /// </summary>
+    public partial class OknoPojazdu : Window
+    {
+        public OknoPojazdu(XmlNode pojazd)
+        {
+            DataContext = pojazd;
+            InitializeComponent();
+        }
+    }
+}
